14474cb1119ce84877260b74925a3f2ea9629a23
chanchanbayub <chanchanbayub@gmail.com>
2024-11-28 10:18:42 +0700

    menghilangkan status paraf pada halaman operator

--- app/Models/Admin/StatusKendaraanModel.php
+++ app/Models/Admin/StatusKendaraanModel.php
@@ -46,6 +46,7 @@ class StatusKendaraanModel extends Model
     {
         return $this->table($this->table)
             ->where(["status_kendaraan_table.id !=" => 2])
+            ->where(["status_kendaraan_table.id !=" => 5])
             ->orderBy('id desc')
             ->get()->getResultObject();
     }
