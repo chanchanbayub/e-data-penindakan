a998d6557da211a71f40d4ab514fb63f0aee6a64
chanchanbayub <chanchanbayub@gmail.com>
2024-12-03 00:30:38 +0700

    menambahkan fitur edit pada halaman petugas

--- app/Models/Petugas/DataPenindakanModel.php
+++ app/Models/Petugas/DataPenindakanModel.php
@@ -19,7 +19,6 @@ class DataPenindakanModel extends Model
 
     public function getDataPenindakan($ukpd_id)
     {
-
         if ($ukpd_id == null) {
             return $this->table($this->table)
                 ->select('data_penindakan_table.id,data_penindakan_table.ukpd_id,data_penindakan_table.nomor_bap ,data_penindakan_table.kode_wilayah_awal, data_penindakan_table.nomor_kendaraan, data_penindakan_table.kode_wilayah_akhir,data_penindakan_table.jenis_kendaraan_id, data_penindakan_table.jenis_penindakan_id, data_penindakan_table.tanggal_penindakan, ukpd_table.ukpd, jenis_kendaraan_table.jenis_kendaraan, tempat_penyimpanan_table.tempat_penyimpanan, jenis_penindakan_table.jenis_penindakan, status_kendaraan_table.status_kendaraan')
@@ -249,5 +248,19 @@ class DataPenindakanModel extends Model
             ->where(["data_penindakan_table.ukpd_id" => $ukpd_id]);
 
         return $builder->orderBy('data_penindakan_table.tanggal_penindakan', 'desc');
+    }
+
+    public function getRowDataPenindakan($id)
+    {
+        return $this->table($this->table)
+            ->select('data_penindakan_table.id,data_penindakan_table.ukpd_id,data_penindakan_table.nomor_bap ,data_penindakan_table.kode_wilayah_awal, data_penindakan_table.nomor_kendaraan, data_penindakan_table.kode_wilayah_akhir,data_penindakan_table.jenis_kendaraan_id, data_penindakan_table.jenis_penindakan_id, data_penindakan_table.tanggal_penindakan, ukpd_table.ukpd, jenis_kendaraan_table.jenis_kendaraan, tempat_penyimpanan_table.tempat_penyimpanan, jenis_penindakan_table.jenis_penindakan, status_kendaraan_table.status_kendaraan, kode_trayek_table.id')
+            ->join('ukpd_table', 'ukpd_table.id = data_penindakan_table.ukpd_id')
+            ->join('jenis_kendaraan_table', 'jenis_kendaraan_table.id = data_penindakan_table.jenis_kendaraan_id')
+            ->join('jenis_penindakan_table', 'jenis_penindakan_table.id = data_penindakan_table.jenis_penindakan_id')
+            ->join('tempat_penyimpanan_table', 'tempat_penyimpanan_table.id = data_penindakan_table.tempat_penyimpanan_id')
+            ->join('status_kendaraan_table', 'status_kendaraan_table.id = data_penindakan_table.status_kendaraan_id')
+            ->where(["data_penindakan_table.id" => $id])
+            ->orderBy('data_penindakan_table.id desc')
+            ->get()->getResultObject();
     }
 }
