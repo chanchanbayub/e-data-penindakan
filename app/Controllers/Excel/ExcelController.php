c6f878e4a35dfd441fb7ed7c0e9919a881488f03
chanchanbayub <chanchanbayub@gmail.com>
2025-01-09 10:30:26 +0700

    menambah kan controller excel

---
+++ app/Controllers/Excel/ExcelController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers\Excel;
+
+use App\Controllers\BaseController;
+use CodeIgniter\HTTP\ResponseInterface;
+
+class ExcelController extends BaseController
+{
+    public function index()
+    {
+        //
+    }
+}
