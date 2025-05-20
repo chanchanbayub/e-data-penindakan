876b59385b7a73fbea2fe9bcf227049f258cee38
chanchanbayub <chanchanbayub@gmail.com>
2025-05-20 10:27:40 +0700

    merubah kop surat berdasarkan tanggal sekarang

--- app/Views/pdf/spk_pdf.php
+++ app/Views/pdf/spk_pdf.php
@@ -170,7 +170,8 @@
                 <td align="center">
                     <p id="p1"> pemerintah daerah khusus ibu kota jakarta</p>
                     <p id="p2"> dinas perhubungan</p>
-                    <?php if (date('Y/m/d') == $pengeluaran->tanggal_keluar) : ?>
+
+                    <?php if (date('Y-m-d') == $pengeluaran->tanggal_keluar) : ?>
                         <p id="p3">Gedung Graha Lestari, Jalan Kesehatan No 48, Kelurahan Petojo Selatan,</p>
                         <p id="p4">Kecamatan Gambir, Kota Administrasi Jakarta Pusat</p>
                     <?php else : ?>
